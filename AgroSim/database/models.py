00a1bdabb83e313f2aadf96a92744518699d7807
Maksym Baran <153942144+jacobstxt@users.noreply.github.com>
2026-05-09 23:07:12 +0300

    Add tbl_users

--- AgroSim/database/models.py
+++ AgroSim/database/models.py
@@ -36,3 +36,11 @@ class SimulationResult(Base):
     time_points      = Column(JSON)
     moisture_data    = Column(JSON)
     temperature_data = Column(JSON)
+
+class User(Base):
+      __tablename__ = "users"
+      id              = Column(Integer, primary_key=True, index=True)
+      email           = Column(String, unique=True, index=True, nullable=False)
+      hashed_password = Column(String, nullable=False)
+      first_name      = Column(String, nullable=True)
+      last_name       = Column(String, nullable=True)
