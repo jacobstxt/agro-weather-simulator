b44f312a5a4b0e19e0e87d2bbb96f53c43d1c06b
Maksym Baran <153942144+jacobstxt@users.noreply.github.com>
2026-05-11 23:00:02 +0300

    feat: add PrivateRoute component for protected navigation

---
+++ AgroSim-frontend/src/components/auth/PrivateRoute.tsx
@@ -0,0 +1,8 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import type { RootState } from '@/store';
+
+export function PrivateRoute() {
+    const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+    return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+}
