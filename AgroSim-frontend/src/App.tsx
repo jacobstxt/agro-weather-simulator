b7cdfa0998d30b2ce983f9e5dbef52d2af41f52e
Maksym Baran <153942144+jacobstxt@users.noreply.github.com>
2026-05-11 23:06:06 +0300

    feat: add /login and /register routes with PrivateRoute protection

--- AgroSim-frontend/src/App.tsx
+++ AgroSim-frontend/src/App.tsx
@@ -1,22 +1,33 @@
 import './App.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {MainLayout} from "./layouts/MainLayout.tsx";
-import {DashboardPage} from "./pages/DashboardPages.tsx";
-import {RegionPage} from "./pages/RegionPage.tsx";
-import {WeatherDataPage} from "./pages/WeatherDataPage.tsx";
-import {ComparisonPage} from "./pages/ComparisonPage.tsx";
-
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { MainLayout } from "./layouts/MainLayout.tsx";
+import { DashboardPage } from "./pages/DashboardPages.tsx";
+import { RegionPage } from "./pages/RegionPage.tsx";
+import { WeatherDataPage } from "./pages/WeatherDataPage.tsx";
+import { ComparisonPage } from "./pages/ComparisonPage.tsx";
+import { LoginPage } from "./pages/LoginPage.tsx";
+import { RegisterPage } from "./pages/RegisterPage.tsx";
+import { PrivateRoute } from "./components/auth/PrivateRoute.tsx";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route element={<MainLayout />}>
-                    <Route path="/" element={<DashboardPage />} />
-                    <Route path="/regions/:id" element={<RegionPage />} />
-                    <Route path="/weather" element={<WeatherDataPage />} />
-                    <Route path="/comparison" element={<ComparisonPage />} />
+                {/* Публічні маршрути */}
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+
+                {/* Захищені маршрути */}
+                <Route element={<PrivateRoute />}>
+                    <Route element={<MainLayout />}>
+                        <Route path="/" element={<DashboardPage />} />
+                        <Route path="/regions/:id" element={<RegionPage />} />
+                        <Route path="/weather" element={<WeatherDataPage />} />
+                        <Route path="/comparison" element={<ComparisonPage />} />
+                    </Route>
                 </Route>
+
+                <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
         </BrowserRouter>
     );
